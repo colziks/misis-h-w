575f0bea02ea75b5e4d2c30acf539d638167bf0b
colziks <114872561+colziks@users.noreply.github.com>
2022-12-13 00:38:41 +0300

    Create 1701a.cpp

---
+++ abramov_i_a/1701a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+
+
+int main() {
+    int t;
+    std::array<int, 17> res{};
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        int a, b, c, d;
+        std::cin >> a >> b >> c >> d;
+        if (a + b + c + d == 4) {
+            res[i] = 2;
+        } else if (a + b + c + d != 0) {
+            res[i] = 1;
+        } else {
+            res[i] = 0;
+        }
+    }
+    for (int i = 0; i < t; i++) {
+        std::cout << res[i] << "\n";
+    }
+}
